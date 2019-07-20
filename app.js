d2b2869dd8186e39fed5999c2b4b8dce43829dca
Steve Burton <steveburtonwebdev@gmail.com>
2019-07-20 15:53:46 +0100

    changed nav colour

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const navSlide = () => {
           if(link.style.animation) {
               link.style.animation =''
           } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.3}s`;
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.2}s`;
           }
             
 
