62f5da5e0ce0554ad202a373f4d23633eec1c8d4
Steve Burton <steveburtonwebdev@gmail.com>
2019-08-05 16:20:37 +0100

    removed mobilenav on click.  fixed bugs

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
+
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navlinks');
-    const navLinks = document.querySelectorAll('.navlinks li');
-    const mainBody = document.querySelector('.mainBody');
-
-    burger.addEventListener('click', ()=> {
-        // toggle nav
-        nav.classList.toggle('nav-active');    
-        // animate links
-        navLinks.forEach((link, index) => {
-          if(link.style.animation) {
-              link.style.animation =''
-          } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.2}s`;
-          }
-        });   
-        // toggle burger icon
-        burger.classList.toggle('toggleBurger');
-    })
-
-
-
+  if (screen.width <= 768) {
+    // toggle nav
+    nav.classList.toggle('nav-active');    
+    // animate links
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation =''
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.2}s`;
+      }
+    });   
+    // toggle burger icon
+    burger.classList.toggle('toggleBurger');
+  }
 }
 
-let imgMoved = false;
-navSlide();
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.navlinks');
+const navLinks = document.querySelectorAll('.navlinks li');
 
+burger.addEventListener('click', navSlide);
+nav.addEventListener('click', navSlide);
